3b188b6b3f58cb794833019030a40691fe10a01d
Smeet Hendre <smeethendre4@gmail.com>
2026-01-08 22:43:29 +0530

    add user schema definition

---
+++ user.model.js
@@ -0,0 +1,40 @@
+import mongoose, { Schema } from "mongoose";
+
+const userSchema = new Schema({
+  userName: {
+    type: String,
+    required: true,
+  },
+
+  password: {
+    type: String,
+    required: true,
+  },
+
+  email: {
+    type: String,
+    required: true,
+  },
+
+  phoneNumber: {
+    type: Number,
+    required: true,
+  },
+
+  numberOfVideos: {
+    type: mongoose.Schema.Type.objectId,
+    ref: "video.model.js",
+  },
+
+  totalNumberOfViews: {
+    type: mongoose.Schema.Type.objectId,
+    ref: "video.model.js",
+  },
+
+  totalSubscribers: {
+    type: Number,
+    required: true,
+  },
+});
+
+export const User = mongoose.model("User", userSchema);
