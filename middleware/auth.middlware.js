8f0b2e80a6624d4e511348abba6502dd3de769ef
Smeet Hendre <smeethendre4@gmail.com>
2026-01-14 23:35:01 +0530

    fix: handle errors in authenticator middleware and return appropriate response

--- middleware/auth.middlware.js
+++ middleware/auth.middlware.js
@@ -1,8 +1,13 @@
 import express from "express";
 
-const authenticator = async(req, res, next) => {
-  try { 
-  } catch (error) {}
+const authenticator = async (req, res, next) => {
+  try {
+  } catch (error) {
+    res.send(400).json({
+      success: false,
+      message: "Unauthorised access",
+    });
+  }
 };
 
 export { authenticator };
