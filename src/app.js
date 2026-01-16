658e1e9689540f906b15d03d3354a5719b0f8848
Smeet Hendre <smeethendre4@gmail.com>
2026-01-16 21:53:37 +0530

    feat: enhance app.js with cookie parser and improved middleware configuration

--- src/app.js
+++ src/app.js
@@ -1,9 +1,29 @@
-import express from "express";
+import express, { json } from "express";
 import cors from "cors";
+import cookieParser from "cookie-parser";
+import { urlencoded } from "express";
+import { json } from "express";
 const app = express();
 
 app.use(
   cors({
-    
+    origin: "https://localhost:500",
+    credentials: true,
   })
 );
+
+app.use(cookieParser());
+
+app.use(
+  urlencoded({
+    limit: "256kb",
+  })
+);
+
+app.use(
+  json({
+    limit: "256kb",
+  })
+);
+
+export { app };
