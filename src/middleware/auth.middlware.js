f22e2026aa2f9cd8ee6a92d91c730557b465d513
Smeet Hendre <smeethendre4@gmail.com>
2026-01-18 17:20:46 +0530

    feat: implement JWT authentication middleware with error handling

--- src/middleware/auth.middlware.js
+++ src/middleware/auth.middlware.js
@@ -1,14 +1,39 @@
 import express from "express";
+import { User } from "../model/user.model.js";
+import jwt from "jsonwebtoken";
+import { ApiError } from "../util/apiError.util.js";
 
-const authenticator = async (req, res, next) => {
+const authenticator = asyncHandler(async (req, res, next) => {
   try {
-     
+    const token =
+      req.cookies?.accessToken ||
+      req.header("Authorization")?.replace("Bearer ", "");
+
+    if (!token) {
+      throw new ApiError(401, "Invalid access token");
+    }
+
+    const verify = jwt.verify(token, process.env.accessToken);
+
+    if (!verify) {
+      throw new ApiError(401, "Incorrect access token");
+    }
+
+    const user = await User.findById(verify._id).select(
+      "-password -refreshToken"
+    );
+
+
+    req.user = user; //updated user credentials so that, doesn't have to authenticate everytime.
+    next();
+
+
   } catch (error) {
     res.send(400).json({
       success: false,
       message: "Unauthorised access",
     });
   }
-};
+});
 
 export { authenticator };
